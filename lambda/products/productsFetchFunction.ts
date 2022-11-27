faf72ef1121611a68538da33429dbb33347abecd
Vinicius Pereira <m.vini846@gmail.com>
2022-11-27 20:58:14 -0300

    deploy from stacks

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,26 +1,31 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
-export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-    const lambdaRequestId = context.awsRequestId
-    const apiRequestId = event.requestContext.requestId
-    console.log(`API GATEWAY Request Id: ${apiRequestId} - Lambda Request Id: ${lambdaRequestId}`)
-    const method = event.httpMethod
-    if (event.resource === "/products") {
-        if (method === "GET") {
-            console.log("GET")
+export async function handler(event: APIGatewayProxyEvent, 
+   context: Context): Promise<APIGatewayProxyResult> {
 
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: "GET products - OK"
-                })
-            }
-        }
-    }
-    return {
-        statusCode: 400,
-        body: JSON.stringify({
-            message: "Bad request"
-        })
-    }
+   const lambdaRequestId = context.awsRequestId
+   const apiRequestId = event.requestContext.requestId
+
+   console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
+   
+   const method = event.httpMethod
+   if (event.resource === "/products") {
+      if (method === 'GET') {
+         console.log('GET')
+
+         return {
+            statusCode: 200,
+            body: JSON.stringify({
+               message: "GET Products - OK"               
+            })
+         }
+      }
+   }
+
+   return {
+      statusCode: 400,
+      body: JSON.stringify({
+         message: "Bad request"
+      })
+   }
 }
--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -1,40 +1,41 @@
-import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'
-import * as cdk from 'aws-cdk-lib'
-import * as apigateway from 'aws-cdk-lib/aws-apigateway'
-import * as cwlogs from 'aws-cdk-lib/aws-logs'
-import { Construct } from 'constructs'
+import * as cdk from "aws-cdk-lib"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as apigateway from "aws-cdk-lib/aws-apigateway"
+import * as cwlogs from "aws-cdk-lib/aws-logs"
+import { Construct } from "constructs"
 
 interface ECommerceApiStackProps extends cdk.StackProps {
-    productsFetchHandler: lambdaNodeJs.NodejsFunction
+   productsFetchHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
-        super(scope, id, props)
 
-        const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
-        const api = new apigateway.RestApi(this, "ECommerceApi", {
-            restApiName: "ECommerceApi",
-            deployOptions: {
-                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
-                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
-                    httpMethod: true, 
-                    ip: true,
-                    protocol: true,
-                    requestTime: true,
-                    resourcePath: true,
-                    responseLength: true,
-                    status: true,
-                    caller: true,
-                    user: true
-                })
-            }
-        })
+   constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
+      super(scope, id, props)
 
-        const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+      const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
+      const api = new apigateway.RestApi(this, "ECommerceApi", {
+         restApiName: "ECommerceApi",
+         deployOptions: {
+            accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+            accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+               httpMethod: true,
+               ip: true,
+               protocol: true,
+               requestTime: true,
+               resourcePath: true,
+               responseLength: true,
+               status: true,
+               caller: true,
+               user: true
+            })
+         }
+      })
 
-        // /products
-        const productsResource = api.root.addResource("products")
-        productsResource.addMethod("GET", productsFetchIntegration)
-    }
+      const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+
+      // "/products"
+      const productsResource = api.root.addResource("products")
+      productsResource.addMethod("GET", productsFetchIntegration)
+   }
 }
